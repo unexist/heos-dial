7e9ac50d77398a769a2586122344af4b0cdb9bd8
Christoph Kappel <christoph@unexist.dev>
2024-09-26 15:49:59 +0200

    Added initial wifi and encoder handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,167 @@
+///
+/// @package geos-dial
+///
+/// @file Main file
+/// @copyright 2024-present Christoph Kappel <christoph@unexist.dev>
+/// @version $Id$
+///
+/// This program can be distributed under the terms of the GNU GPLv2.
+/// See the file LICENSE for details.
+///
+
+use anyhow::{bail, Result};
+use core::str;
+use embedded_svc::{
+    http::{client::Client, Method},
+    io::Read,
+};
+use esp_idf_svc::{
+    eventloop::EspSystemEventLoop,
+    hal::prelude::Peripherals,
+    http::client::{Configuration, EspHttpConnection},
+};
+use wifi::wifi;
+
+#[toml_cfg::toml_config]
+pub struct Config {
+    #[default("serc")]
+    wifi_ssid: &'static str,
+    #[default("")]
+    wifi_psk: &'static str,
+}
+
+#[cfg(any(esp32, esp32s2, esp32s3))]
+fn main() -> anyhow::Result<()> {
+    use anyhow::Context;
+    use encoder::Encoder;
+    use esp_idf_hal::delay::FreeRtos;
+    use esp_idf_hal::prelude::*;
+
+    esp_idf_hal::sys::link_patches();
+
+    println!("setup pins");
+    let peripherals = Peripherals::take().context("failed to take Peripherals")?;
+    let mut pin_a = peripherals.pins.gpio5;
+    let mut pin_b = peripherals.pins.gpio6;
+
+    println!("setup encoder");
+    let encoder = Encoder::new(peripherals.pcnt0, &mut pin_a, &mut pin_b)?;
+
+    let mut last_value = 0i32;
+    loop {
+        let value = encoder.get_value()?;
+
+        if value != last_value {
+            println!("value: {value}");
+            last_value = value;
+        }
+        FreeRtos::delay_ms(100u32);
+    }
+}
+
+#[cfg(not(any(esp32, esp32s2, esp32s3)))]
 fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+    use esp_idf_hal::delay::FreeRtos;
+    println!("pcnt peripheral not supported on this device!");
+    loop {
+        FreeRtos::delay_ms(100u32);
+    }
+}
 
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
+#[cfg(any(esp32, esp32s2, esp32s3))]
+// esp-idf encoder implementation using v4 pcnt api
+mod encoder {
+    use std::cmp::min;
+    use std::sync::atomic::AtomicI32;
+    use std::sync::atomic::Ordering;
+    use std::sync::Arc;
 
-    log::info!("Hello, world!");
+    use esp_idf_hal::gpio::AnyInputPin;
+    use esp_idf_hal::gpio::InputPin;
+    use esp_idf_hal::pcnt::*;
+    use esp_idf_hal::peripheral::Peripheral;
+    use esp_idf_sys::EspError;
+
+    const LOW_LIMIT: i16 = -100;
+    const HIGH_LIMIT: i16 = 100;
+
+    pub struct Encoder<'d> {
+        unit: PcntDriver<'d>,
+        approx_value: Arc<AtomicI32>,
+    }
+
+    impl<'d> Encoder<'d> {
+        pub fn new<PCNT: Pcnt>(
+            pcnt: impl Peripheral<P = PCNT> + 'd,
+            pin_a: impl Peripheral<P = impl InputPin> + 'd,
+            pin_b: impl Peripheral<P = impl InputPin> + 'd,
+        ) -> Result<Self, EspError> {
+            let mut unit = PcntDriver::new(
+                pcnt,
+                Some(pin_a),
+                Some(pin_b),
+                Option::<AnyInputPin>::None,
+                Option::<AnyInputPin>::None,
+            )?;
+            unit.channel_config(
+                PcntChannel::Channel0,
+                PinIndex::Pin0,
+                PinIndex::Pin1,
+                &PcntChannelConfig {
+                    lctrl_mode: PcntControlMode::Reverse,
+                    hctrl_mode: PcntControlMode::Keep,
+                    pos_mode: PcntCountMode::Decrement,
+                    neg_mode: PcntCountMode::Increment,
+                    counter_h_lim: HIGH_LIMIT,
+                    counter_l_lim: LOW_LIMIT,
+                },
+            )?;
+            unit.channel_config(
+                PcntChannel::Channel1,
+                PinIndex::Pin1,
+                PinIndex::Pin0,
+                &PcntChannelConfig {
+                    lctrl_mode: PcntControlMode::Reverse,
+                    hctrl_mode: PcntControlMode::Keep,
+                    pos_mode: PcntCountMode::Increment,
+                    neg_mode: PcntCountMode::Decrement,
+                    counter_h_lim: HIGH_LIMIT,
+                    counter_l_lim: LOW_LIMIT,
+                },
+            )?;
+
+            unit.set_filter_value(min(10 * 80, 1023))?;
+            unit.filter_enable()?;
+
+            let approx_value = Arc::new(AtomicI32::new(0));
+            // unsafe interrupt code to catch the upper and lower limits from the encoder
+            // and track the overflow in `value: Arc<AtomicI32>` - I plan to use this for
+            // a wheeled robot's odomerty
+            unsafe {
+                let approx_value = approx_value.clone();
+                unit.subscribe(move |status| {
+                    let status = PcntEventType::from_repr_truncated(status);
+                    if status.contains(PcntEvent::HighLimit) {
+                        approx_value.fetch_add(HIGH_LIMIT as i32, Ordering::SeqCst);
+                    }
+                    if status.contains(PcntEvent::LowLimit) {
+                        approx_value.fetch_add(LOW_LIMIT as i32, Ordering::SeqCst);
+                    }
+                })?;
+            }
+            unit.event_enable(PcntEvent::HighLimit)?;
+            unit.event_enable(PcntEvent::LowLimit)?;
+            unit.counter_pause()?;
+            unit.counter_clear()?;
+            unit.counter_resume()?;
+
+            Ok(Self { unit, approx_value })
+        }
+
+        pub fn get_value(&self) -> Result<i32, EspError> {
+            let value =
+                self.approx_value.load(Ordering::Relaxed) + self.unit.get_counter_value()? as i32;
+            Ok(value)
+        }
+    }
 }
