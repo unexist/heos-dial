e0a15f8801e44a29183299a487dfa8ba14dae653
Christoph Kappel <christoph@unexist.dev>
2024-09-26 15:49:00 +0200

    Added file header

--- build.rs
+++ build.rs
@@ -1,3 +1,14 @@
+///
+/// @package heos-dial
+///
+/// @file Build file
+/// @copyright 2024-present Christoph Kappel <christoph@unexist.dev>
+/// @version $Id$
+///
+/// This program can be distributed under the terms of the GNU GPLv2.
+/// See the file LICENSE for details.
+///
+
 fn main() {
     embuild::espidf::sysenv::output();
 }
