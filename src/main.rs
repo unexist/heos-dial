e98278ff960c4c73d96dc01a21924aa7800a674e
Christoph Kappel <christoph@unexist.dev>
2024-09-27 16:10:25 +0200

    Updated code

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,16 @@
 use anyhow::{bail, Result};
 use core::str;
 use embedded_svc::{
-    http::{client::Client, Method},
     io::Read,
 };
 use esp_idf_svc::{
     eventloop::EspSystemEventLoop,
-    hal::prelude::Peripherals,
-    http::client::{Configuration, EspHttpConnection},
+};
+use esp_idf_svc::hal::{
+    delay::FreeRtos,
+    peripherals::Peripherals,
+    wifi::wifi,
+    prelude::*,
 };
 use wifi::wifi;
 
@@ -34,29 +37,46 @@ pub struct Config {
 fn main() -> anyhow::Result<()> {
     use anyhow::Context;
     use encoder::Encoder;
-    use esp_idf_hal::delay::FreeRtos;
-    use esp_idf_hal::prelude::*;
 
-    esp_idf_hal::sys::link_patches();
+    esp_idf_svc::sys::link_patches();
+    esp_idf_svc::log::EspLogger::initialize_default();
 
-    println!("setup pins");
-    let peripherals = Peripherals::take().context("failed to take Peripherals")?;
+    let app_config = CONFIG;
+    let sysloop = EspSystemEventLoop::take()?;
+
+    println!("Setup pins");
+    let peripherals = Peripherals::take().context("Failed to take Peripherals")?;
     let mut pin_a = peripherals.pins.gpio5;
     let mut pin_b = peripherals.pins.gpio6;
 
-    println!("setup encoder");
+    println!("Setup encoder");
     let encoder = Encoder::new(peripherals.pcnt0, &mut pin_a, &mut pin_b)?;
-
     let mut last_value = 0i32;
-    loop {
-        let value = encoder.get_value()?;
 
-        if value != last_value {
-            println!("value: {value}");
-            last_value = value;
+    // Connect to the Wi-Fi network
+    let _wifi = match wifi(
+        app_config.wifi_ssid,
+        app_config.wifi_psk,
+        peripherals.modem,
+        sysloop,
+    ) {
+        Ok(inner) => {
+            println!("Connected to Wi-Fi network!");
+            let value = encoder.get_value()?;
+
+            if value != last_value {
+                println!("value: {value}");
+                last_value = value;
+            }
+            FreeRtos::delay_ms(100u32);
+            inner
         }
-        FreeRtos::delay_ms(100u32);
-    }
+        Err(err) => {
+            // Red!
+            bail!("Could not connect to Wi-Fi network: {:?}", err)
+        }
+    };
+    Ok(())
 }
 
 #[cfg(not(any(esp32, esp32s2, esp32s3)))]
@@ -75,11 +95,10 @@ mod encoder {
     use std::sync::atomic::AtomicI32;
     use std::sync::atomic::Ordering;
     use std::sync::Arc;
-
-    use esp_idf_hal::gpio::AnyInputPin;
-    use esp_idf_hal::gpio::InputPin;
-    use esp_idf_hal::pcnt::*;
-    use esp_idf_hal::peripheral::Peripheral;
+    use esp_idf_svc::hal::peripheral::Peripheral;
+    use esp_idf_svc::hal::gpio::AnyInputPin;
+    use esp_idf_svc::hal::gpio::InputPin;
+    use esp_idf_svc::hal::pcnt::*;
     use esp_idf_sys::EspError;
 
     const LOW_LIMIT: i16 = -100;
