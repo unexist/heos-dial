9a1d43bf845a8465a67ab1c8fbb46cca27107a18
Christoph Kappel <christoph@unexist.dev>
2024-09-29 14:24:05 +0200

    Added check for config

--- build.rs
+++ build.rs
@@ -10,5 +10,10 @@
 ///
 
 fn main() {
+    // Check if the `cfg.toml` file exists and has been filled out.
+    if !std::path::Path::new("cfg.toml").exists() {
+        panic!("You need to create a `cfg.toml` file with your Wi-Fi credentials! Use `cfg.toml.example` as a template.");
+    }
+
     embuild::espidf::sysenv::output();
 }
