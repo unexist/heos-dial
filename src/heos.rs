75faa2789089f0b15cfb8c4239993bb735e64c8e
Christoph Kappel <christoph@unexist.dev>
2024-09-30 17:33:03 +0200

    Updated heos handler

--- src/heos.rs
+++ src/heos.rs
@@ -9,6 +9,8 @@
 /// See the file LICENSE for details.
 ///
 
+use std::net::UdpSocket;
+
 const TargetUrn: &'static str = "urn:schemas-denon-com:device:ACT-Denon:1";
 const DiscoverMessage: &'static str = "M-SEARCH * HTTP/1.1\r\n \
     HOST: 239.255.255.250:1900\r\n \
@@ -17,6 +19,22 @@ const DiscoverMessage: &'static str = "M-SEARCH * HTTP/1.1\r\n \
     MAN: \"ssdp:discover\"\r\n \
     \r\n";
 
-pub fn Heos() -> anyhow::Result<()> {
-    Ok(())
+pub struct Heos<'d> {
+    socket: UdpSocket,
+}
+
+impl<'d> Heos<'d> {
+    pub fn new(socket: UdpSocket) -> Self {
+        Self { socket }
+    }
+
+    pub fn discovery() -> Self {
+        let mut buf = [0; 10];
+        buf.reverse();
+
+        socket.send_to(buf, &src)?;
+
+        let buf = &mut buf[..amt];
+        let (amt, src) = socket.recv_from(&mut buf)?;
+    }
 }
