34d2103e4820ce120920efd496287ffee3143386
Christoph Kappel <christoph@unexist.dev>
2024-09-30 11:53:30 +0200

    Added init heos handler

---
+++ src/heos.rs
@@ -0,0 +1,14 @@
+///
+/// @package heos-dial
+///
+/// @file HEOS protocol helper
+/// @copyright 2024-present Christoph Kappel <christoph@unexist.dev>
+/// @version $Id$
+///
+/// This program can be distributed under the terms of the GNU GPLv3.
+/// See the file LICENSE for details.
+///
+
+pub fn Heos() -> anyhow::Result<()> {
+    Ok(())
+}
