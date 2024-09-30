8b3bef31bc3ed6718258af1000980c690eb4cd81
Christoph Kappel <christoph@unexist.dev>
2024-09-30 16:34:25 +0200

    Added discovery constants

--- src/heos.rs
+++ src/heos.rs
@@ -9,6 +9,14 @@
 /// See the file LICENSE for details.
 ///
 
+const TargetUrn: &'static str = "urn:schemas-denon-com:device:ACT-Denon:1";
+const DiscoverMessage: &'static str = "M-SEARCH * HTTP/1.1\r\n \
+    HOST: 239.255.255.250:1900\r\n \
+    ST: {urn}\r\n \
+    MX: 5\r\n \
+    MAN: \"ssdp:discover\"\r\n \
+    \r\n";
+
 pub fn Heos() -> anyhow::Result<()> {
     Ok(())
 }
