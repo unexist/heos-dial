1f4d824b04e98d2a5e1ace3c1dffc4ceb8d720a4
Christoph Kappel <christoph@unexist.dev>
2024-09-30 17:32:29 +0200

    Refactored wifi handler

--- src/wifi.rs
+++ src/wifi.rs
@@ -17,33 +17,18 @@ use esp_idf_svc::{
 };
 use log::info;
 
-pub fn Wifi(
+pub fn create_wifi(
     ssid: &str,
     pass: &str,
     modem: impl peripheral::Peripheral<P = esp_idf_svc::hal::modem::Modem> + 'static,
     sysloop: EspSystemEventLoop,
-) -> Result<Box<EspWifi<'static>>> {
+) -> Result<EspWifi<'static>> {
     if ssid.is_empty() || pass.is_empty() {
         bail!("Missing WiFi name or password")
     }
 
-    let mut auth_method = AuthMethod::WPA2Personal;
     let mut esp_wifi = EspWifi::new(modem, sysloop.clone(), None)?;
     let mut wifi = BlockingWifi::wrap(&mut esp_wifi, sysloop)?;
-
-    info!("Starting wifi scan");
-
-    wifi.set_configuration(&Configuration::Client(ClientConfiguration::default()))?;
-    wifi.start()?;
-
-    let ap_infos = wifi.scan()?;
-    let ours = ap_infos.into_iter().find(|a| a.ssid == ssid);
-
-    let channel = if let Some(ours) = ours {
-        Some(ours.channel)
-    } else {
-        None
-    };
 
     wifi.set_configuration(&Configuration::Client(ClientConfiguration {
         ssid: ssid
@@ -52,22 +37,17 @@ pub fn Wifi(
         password: pass
             .try_into()
             .expect("Could not parse the given password into WiFi config"),
-        channel,
-        auth_method,
         ..Default::default()
     }))?;
 
-    info!("Connecting wifi...");
-
     wifi.connect()?;
-
-    info!("Waiting for DHCP lease...");
+    info!("Wifi connected");
 
     wifi.wait_netif_up()?;
+    info!("Wifi Netif up");
 
     let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
-
     info!("Wifi DHCP info: {:?}", ip_info);
 
-    Ok(Box::new(esp_wifi))
+    Ok(esp_wifi)
 }
