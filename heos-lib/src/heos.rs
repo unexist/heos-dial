4af8257389db5464710f02fe09586ddc23ce8ca3
Christoph Kappel <christoph@unexist.dev>
2024-10-25 16:00:24 +0200

    Fixed multiline strings

--- heos-lib/src/heos.rs
+++ heos-lib/src/heos.rs
@@ -21,11 +21,11 @@ use crate::heos_device::HeosDevice;
 const PREFIX: &'static str = "heos://";
 const POSTFIX: &'static str = "\r\n";
 const TARGET_URN: &'static str = "urn:schemas-denon-com:device:ACT-Denon:1";
-const DISCOVERY_REQUEST: &'static str = formatcp!(r#"M-SEARCH * HTTP/1.1\r\n
-HOST: 239.255.255.250:1900\r\n
-MAN: "ssdp:discover"\r\n
-MX: 5\r\n
-ST: {urn}\r\n\r\n"#, urn = TARGET_URN);
+const DISCOVERY_REQUEST: &'static str = formatcp!("M-SEARCH * HTTP/1.1\r\n\
+HOST: 239.255.255.250:1900\r\n\
+ST: {urn}\r\n\
+MX: 5\r\n\
+MAN: \"ssdp:discover\"\r\n\r\n" , urn = TARGET_URN);
 
 #[derive(Default)]
 pub struct Heos {
@@ -69,10 +69,7 @@ impl Heos {
                     let (size, _) = socket.recv_from(&mut buf).await?;
 
                     // Convert the response to a string
-                    let response =
-                        str::from_utf8(unsafe { std::slice::from_raw_parts(buf.as_ptr() as *const u8, size) })?;
-
-                    println!("{}", response);
+                    let response = str::from_utf8(&buf[..size])?;
 
                     if response.contains(TARGET_URN) {
                         Ok(response.to_string())
@@ -113,9 +110,9 @@ impl Heos {
     }
 
     pub(crate) fn parse_discovery_response(response_str: &str) -> Result<HeosDevice> {
-        match response_str.split("\\r\\n\\r\\n").next() {
+        match response_str.split("\r\n\r\n").next() {
             Some(header_str) => {
-                for header_line in header_str.split("\\r\\n") {
+                for header_line in header_str.split("\r\n") {
                     if header_line.contains("LOCATION") {
                         if let Some(idx) = header_line.find(":") {
                             let url = header_line[idx + 1..].trim();
