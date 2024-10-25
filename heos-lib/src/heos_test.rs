38e22b35005e72656e2280d0b9e45470e1ee77b4
Christoph Kappel <christoph@unexist.dev>
2024-10-25 16:34:46 +0200

    Disabled discovery test for now

--- heos-lib/src/heos_test.rs
+++ heos-lib/src/heos_test.rs
@@ -18,20 +18,20 @@ mod heos_test {
     #[test]
     fn should_parse_discovery_response() {
         const LOCATION: &'static str = "http://10.0.8.87:60006/upnp/desc/aios_device/aios_device.xml";
-        const RAW_REPLY: &'static str = formatcp!(r#"HTTP/1.1 200 OK\r\n
-CACHE-CONTROL: max-age=180\r\n
-EXT:\r\n
-LOCATION: {location}\r\n
-VERSIONS.UPNP.HEOS.COM: 10,668205267,801394619,363364703,1840750642,105553199,-316033077,1711326982,-838802320,-170053632,363364703\r\n
-NETWORKID.UPNP.HEOS.COM: d424dda645d7\r\n
-BOOTID.UPNP.ORG: 1947595085\r\n
-IPCACHE.URL.UPNP.HEOS.COM: /ajax/upnp/get_device_info\r\n
-SERVER: LINUX UPnP/1.0 Denon-Heos/316763a47eba7769d9be106fb4f3617c5393a2b7\r\n
-ST: urn:schemas-denon-com:device:ACT-Denon:1\r\n
-USN: uuid:60f346a0-9018-49e7-b77e-4a14ad25b96f::urn:schemas-denon-com:device:ACT-Denon:1\r\n\r\n"#, location = LOCATION);
+        const RAW_REPLY: &'static str = formatcp!("HTTP/1.1 200 OK\r\n\
+CACHE-CONTROL: max-age=180\r\n\
+EXT:\r\n\
+LOCATION: {location}\r\n\
+VERSIONS.UPNP.HEOS.COM: 10,668205267,801394619,363364703,1840750642,105553199,-316033077,1711326982,-838802320,-170053632,363364703\r\n\
+NETWORKID.UPNP.HEOS.COM: d424dda645d7\r\n\
+BOOTID.UPNP.ORG: 1947595085\r\n\
+IPCACHE.URL.UPNP.HEOS.COM: /ajax/upnp/get_device_info\r\n\
+SERVER: LINUX UPnP/1.0 Denon-Heos/316763a47eba7769d9be106fb4f3617c5393a2b7\r\n\
+ST: urn:schemas-denon-com:device:ACT-Denon:1\r\n\
+USN: uuid:60f346a0-9018-49e7-b77e-4a14ad25b96f::urn:schemas-denon-com:device:ACT-Denon:1\r\n\r\n", location = LOCATION);
 
         match Heos::parse_discovery_response(RAW_REPLY) {
-            Ok(device) => assert!(LOCATION.eq(&device._url)),
+            Ok(device) => assert!(LOCATION.eq(&device.base_url)),
             Err(err) => panic!("Assertion failed: {}", err),
         }
     }
@@ -55,6 +55,7 @@ USN: uuid:60f346a0-9018-49e7-b77e-4a14ad25b96f::urn:schemas-denon-com:device:ACT
                                       "get_volume", vec![("pid", "5")]), COMMAND);
     }
 
+    #[ignore]
     #[tokio::test]
     async fn should_discover_anything() {
         let heos = Heos::new();
@@ -64,8 +65,9 @@ USN: uuid:60f346a0-9018-49e7-b77e-4a14ad25b96f::urn:schemas-denon-com:device:ACT
 
         if let Some(device) = devices.next().await {
             println!("{:?}", device);
+            assert!(true);
+        } else {
+            assert!(false, "Failed to discover devices");
         }
-
-        assert!(true);
     }
 }
