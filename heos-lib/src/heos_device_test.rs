26eb3e16555ca0290015142e3f80593d35c6c62e
Christoph Kappel <christoph@unexist.dev>
2024-11-06 11:17:14 +0100

    Added test for #send

--- heos-lib/src/heos_device_test.rs
+++ heos-lib/src/heos_device_test.rs
@@ -12,11 +12,26 @@
 #[cfg(test)]
 mod heos_device_test {
     use crate::constants::LOCATION;
+    use crate::heos_command::HeosCommand;
     use crate::HeosDevice;
 
     fn should_create_valid_client() {
         let dev = HeosDevice::new(LOCATION, "1");
 
         assert!(dev.is_ok());
+    }
+
+    #[ignore]
+    #[tokio::test]
+    async fn should_connect_and_get_result() {
+        let dev = HeosDevice::new(LOCATION, "1")
+            .expect("Failed to create client");
+
+        let cmd = dev.command_from("player", "get_players", vec![])
+            .expect("Failed to create command");
+
+        let result = dev.send(cmd.as_ref()).await;
+
+        assert!(result.is_ok());
     }
 }
