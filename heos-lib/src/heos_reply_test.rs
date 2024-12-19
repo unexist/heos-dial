4667a325f2655d802a6d5c10b6cf2040b4de54c6
Christoph Kappel <christoph@unexist.dev>
2024-12-19 12:21:26 +0100

    Renamed constants

--- heos-lib/src/heos_reply_test.rs
+++ heos-lib/src/heos_reply_test.rs
@@ -13,44 +13,44 @@
 mod heos_reply_test {
     use crate::heos_reply::HeosReply;
 
-    const RAW_GET_PLAYERS_REPLY: &'static str = r###"{"heos": {"command": "player/get_players",\
+    const JSON_GET_PLAYERS_REPLY: &'static str = r###"{"heos": {"command": "player/get_players",\
 "result": "success", "message": ""},\
 "payload": [\
 {"name": "Living Room (AVR)", "pid": -474905601, "model": "Denon AVR-S660H", "version": "3.34.410", "ip": "10.0.8.37", "network": "wired", "lineout": 0, "serial": "DBNM052317669"},\
 {"name": "Studio1", "pid": 844263156, "gid": -622728288, "model": "Denon Home 350", "version": "3.34.425", "ip": "10.0.8.24", "network": "wifi", "lineout": 0, "serial": "BME27220818140"}\
 ]}\r\n"###;
 
-    const RAW_SET_PLAY_STATE_REPLY: &'static str = r###"{"heos": {\
+    const JSON_SET_PLAY_STATE_REPLY: &'static str = r###"{"heos": {\
 "command": "player/get_play_state",\
 "result": "success",\
 "message": "pid='player_id'&state='play_state'"\
 }\r\n"###;
 
-    const RAW_GET_PLAY_STATE_REPLY: &'static str = r###"{"heos": {\
+    const JSON_GET_PLAY_STATE_REPLY: &'static str = r###"{"heos": {\
 "command": "player/set_play_state", \
 "result": "success", \
 "message": "message": "pid='player_id'&state='play_state'" \
 }\r\n"###;
 
-    const RAW_SET_VOLUME_REPLY: &'static str = r###"{"heos": {\
+    const JSON_SET_VOLUME_REPLY: &'static str = r###"{"heos": {\
 "command": "player/set_volume",\
 "result": "success",\
 "message": "pid='player_id'&level='vol_level'"
 }\r\n"###;
 
-    const RAW_GET_VOLUME_REPLY: &'static str = r###"{"heos": {\
+    const JSON_GET_VOLUME_REPLY: &'static str = r###"{"heos": {\
 "command": "player/get_volume",\
 "result": "success",\
 "message": "pid='player_id'&level='vol_level'"
 }\r\n"###;
 
-    const RAW_PLAY_NEXT_REPLY: &'static str = r###"{"heos": {\
+    const JSON_PLAY_NEXT_REPLY: &'static str = r###"{"heos": {\
 "command": " player/play_next", \
 "result": "success", \
 "message": "pid=player_id" \
 }\r\n"###;
 
-    const RAW_PLAY_PREVIOUS_REPLY: &'static str = r###"{"heos": {\
+    const JSON_PLAY_PREVIOUS_REPLY: &'static str = r###"{"heos": {\
 "command": " player/play_next", \
 "result": "success", \
 "message": "pid=player_id" \
@@ -58,7 +58,7 @@ mod heos_reply_test {
 
     #[test]
     fn should_parse_get_players_reply() {
-        let reply = HeosReply::parse(RAW_GET_PLAYERS_REPLY)
+        let reply = HeosReply::parse(JSON_GET_PLAYERS_REPLY)
             .expect("Failed to parse");
 
         assert!(matches!(reply, HeosReply::Players { .. }));
@@ -66,7 +66,7 @@ mod heos_reply_test {
 
     #[test]
     fn should_parse_set_play_state_reply() {
-        let reply = HeosReply::parse(RAW_SET_PLAY_STATE_REPLY)
+        let reply = HeosReply::parse(JSON_SET_PLAY_STATE_REPLY)
             .expect("Failed to parse");
 
         assert!(matches!(reply, HeosReply::PlayState { .. }));
@@ -74,7 +74,7 @@ mod heos_reply_test {
 
     #[test]
     fn should_parse_get_play_state_reply() {
-        let reply = HeosReply::parse(RAW_GET_PLAY_STATE_REPLY)
+        let reply = HeosReply::parse(JSON_GET_PLAY_STATE_REPLY)
             .expect("Failed to parse");
 
         assert!(matches!(reply, HeosReply::PlayState { .. }));
@@ -82,7 +82,7 @@ mod heos_reply_test {
 
     #[test]
     fn should_parse_set_volume_reply() {
-        let reply = HeosReply::parse(RAW_SET_VOLUME_REPLY)
+        let reply = HeosReply::parse(JSON_SET_VOLUME_REPLY)
             .expect("Failed to parse");
 
         assert!(matches!(reply, HeosReply::Volume { .. }));
@@ -90,7 +90,7 @@ mod heos_reply_test {
 
     #[test]
     fn should_parse_get_volume_reply() {
-        let reply = HeosReply::parse(RAW_GET_VOLUME_REPLY)
+        let reply = HeosReply::parse(JSON_GET_VOLUME_REPLY)
             .expect("Failed to parse");
 
         assert!(matches!(reply, HeosReply::Volume { .. }));
@@ -98,7 +98,7 @@ mod heos_reply_test {
 
     #[test]
     fn should_parse_play_next_reply() {
-        let reply = HeosReply::parse(RAW_PLAY_NEXT_REPLY)
+        let reply = HeosReply::parse(JSON_PLAY_NEXT_REPLY)
             .expect("Failed to parse");
 
         assert!(matches!(reply, HeosReply::PlayAction { .. }));
@@ -106,7 +106,7 @@ mod heos_reply_test {
 
     #[test]
     fn should_parse_play_previous_reply() {
-        let reply = HeosReply::parse(RAW_PLAY_PREVIOUS_REPLY)
+        let reply = HeosReply::parse(JSON_PLAY_PREVIOUS_REPLY)
             .expect("Failed to parse");
 
         assert!(matches!(reply, HeosReply::PlayAction { .. }));
