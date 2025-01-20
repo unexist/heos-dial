5cf8e36936ffb65918c3def392d9e6615630f647
Christoph Kappel <christoph@unexist.dev>
2025-01-20 12:51:53 +0100

    Added tui skeleton

---
+++ heos-tui/src/main.rs
@@ -0,0 +1,25 @@
+//
+/// @package heos-dial
+///
+/// @file HEOS tui
+/// @copyright 2024-present Christoph Kappel <christoph@unexist.dev>
+/// @version $Id$
+///
+/// This program can be distributed under the terms of the GNU GPLv3.
+/// See the file LICENSE for details.
+///
+
+pub use app::App;
+
+pub mod app;
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    let terminal = ratatui::init();
+    let result = App::new().run(terminal);
+
+    ratatui::restore();
+
+    result
+}
