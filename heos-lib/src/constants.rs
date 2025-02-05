67c9880c929b279a2e05fef8bb91cdbe0a4e4e5d
Christoph Kappel <christoph@unexist.dev>
2025-02-05 08:50:04 +0100

    Removed TEST_DEVICE_NAME and TEST_DEVICE_IP

--- heos-lib/src/constants.rs
+++ heos-lib/src/constants.rs
@@ -19,11 +19,5 @@ pub(crate) const CMD_POSTFIX: &'static str = "\r\n";
 pub(crate) const TARGET_URN: &'static str = "urn:schemas-denon-com:device:ACT-Denon:1";
 
 #[cfg(test)]
-pub(crate) const TEST_DEVICE_NAME: &'static str = "Studio1";
-#[cfg(test)]
-pub(crate) const TEST_DEVICE_IP: &'static str = "10.0.8.24";
-#[cfg(test)]
-pub(crate) const TEST_DEVICE_PLAYER_ID: &'static str = "844263156";
-#[cfg(test)]
 pub(crate) const TEST_LOCATION_STR: &'static str =
     formatcp!("http://{ip}:60006/upnp/desc/aios_device/aios_device.xml", ip = TEST_DEVICE_IP);
