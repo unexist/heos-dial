09a445698c16ccc9463fe31a690a1e82618ee607
Christoph Kappel <christoph@unexist.dev>
2025-02-24 17:52:00 +0100

    Added event handlers

---
+++ heos-tui/src/handlers.rs
@@ -0,0 +1,46 @@
+///
+/// @package heos-dial
+///
+/// @file HEOS tui
+/// @copyright 2024-present Christoph Kappel <christoph@unexist.dev>
+/// @version $Id$
+///
+/// This program can be distributed under the terms of the GNU GPLv3.
+/// See the file LICENSE for details.
+///
+
+use crate::app::{App, AppResult};
+use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
+
+pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
+    match key_event.code {
+        KeyCode::Char('h') | KeyCode::Left => {
+            app.select_none();
+            app.decrement_counter();
+        },
+        KeyCode::Char('j') | KeyCode::Down => app.select_next(),
+        KeyCode::Char('k') | KeyCode::Up => app.select_previous(),
+        KeyCode::Char('g') | KeyCode::Home => app.select_first(),
+        KeyCode::Char('G') | KeyCode::End => app.select_last(),
+        KeyCode::Char('l') | KeyCode::Right | KeyCode::Enter => {
+            app.toggle_status();
+            app.increment_counter();
+        },
+
+        KeyCode::Esc | KeyCode::Char('q') => app.quit(),
+
+        // Exit application on `Ctrl-C`
+        KeyCode::Char('c') | KeyCode::Char('C') => {
+            if key_event.modifiers == KeyModifiers::CONTROL {
+                app.quit();
+            }
+        },
+
+        _ => {}
+    }
+    Ok(())
+}
+
+
+
+
