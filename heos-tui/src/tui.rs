5b954299860a4a1c5f5f31277777b38f248a3d19
Christoph Kappel <christoph@unexist.dev>
2025-02-24 17:52:11 +0100

    Added tui handler

---
+++ heos-tui/src/tui.rs
@@ -0,0 +1,73 @@
+///
+/// @package heos-dial
+///
+/// @file HEOS tui
+/// @copyright 2024-present Christoph Kappel <christoph@unexist.dev>
+/// @version $Id$
+///
+/// This program can be distributed under the terms of the GNU GPLv3.
+/// See the file LICENSE for details.
+///
+
+use crate::app::{App, AppResult};
+use crossterm::event::{DisableMouseCapture, EnableMouseCapture};
+use crossterm::terminal::{self, EnterAlternateScreen, LeaveAlternateScreen};
+use ratatui::backend::Backend;
+use ratatui::Terminal;
+use std::io;
+use std::panic;
+use crate::events::EventHandler;
+
+#[derive(Debug)]
+pub struct Tui<B: Backend> {
+    terminal: Terminal<B>,
+    pub events: EventHandler,
+}
+
+impl<B: Backend> Tui<B> {
+    pub fn new(terminal: Terminal<B>, events: EventHandler) -> Self {
+        Self {
+            terminal,
+            events }
+    }
+
+    pub fn init(&mut self) -> AppResult<()> {
+        terminal::enable_raw_mode()?;
+        crossterm::execute!(io::stdout(), EnterAlternateScreen, EnableMouseCapture)?;
+
+        // Define a custom panic hook to reset the terminal properties.
+        // This way, you won't have your terminal messed up if an unexpected error happens.
+        let panic_hook = panic::take_hook();
+        panic::set_hook(Box::new(move |panic| {
+            Self::reset().expect("Failed to reset the terminal");
+            panic_hook(panic);
+        }));
+
+        self.terminal.hide_cursor()?;
+        self.terminal.clear()?;
+
+        Ok(())
+    }
+
+    pub fn draw(&mut self, app: &mut App) -> AppResult<()> {
+        self.terminal.draw(|frame|
+            frame.render_widget(app, frame.area())
+        )?;
+
+        Ok(())
+    }
+
+    fn reset() -> AppResult<()> {
+        terminal::disable_raw_mode()?;
+        crossterm::execute!(io::stdout(), LeaveAlternateScreen, DisableMouseCapture)?;
+
+        Ok(())
+    }
+
+    pub fn exit(&mut self) -> AppResult<()> {
+        Self::reset()?;
+        self.terminal.show_cursor()?;
+
+        Ok(())
+    }
+}
