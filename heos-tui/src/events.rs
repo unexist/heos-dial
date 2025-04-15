b9e797a60b091c449b6d3aa5c597991b1dd78683
Christoph Kappel <christoph@unexist.dev>
2025-04-15 13:36:24 +0200

    Tided up code

--- heos-tui/src/events.rs
+++ heos-tui/src/events.rs
@@ -29,7 +29,7 @@ pub enum Event {
 #[allow(dead_code)]
 #[derive(Debug)]
 pub struct EventHandler {
-    sender: mpsc::UnboundedSender<Event>,
+    pub sender: mpsc::UnboundedSender<Event>,
     receiver: mpsc::UnboundedReceiver<Event>,
     handler: tokio::task::JoinHandle<()>,
 }
@@ -38,30 +38,32 @@ impl EventHandler {
     pub fn new(tick_rate: u64) -> Self {
         let tick_rate = Duration::from_millis(tick_rate);
         let (sender, receiver) = mpsc::unbounded_channel();
-        let _sender = sender.clone();
+        let cloned_sender = sender.clone();
 
         let handler = tokio::spawn(async move {
             let mut reader = crossterm::event::EventStream::new();
             let mut tick = tokio::time::interval(tick_rate);
+
             loop {
                 let tick_delay = tick.tick();
                 let crossterm_event = reader.next().fuse();
+
                 tokio::select! {
-                  _ = _sender.closed() => {
-                    break;
+                  _ = cloned_sender.closed() => {
+                        break;
                   }
                   _ = tick_delay => {
-                    _sender.send(Event::Tick).unwrap();
+                        cloned_sender.send(Event::Tick).unwrap();
                   }
                   Some(Ok(evt)) = crossterm_event => {
                         match evt {
                             CrosstermEvent::Key(key) => {
                                 if key.kind == crossterm::event::KeyEventKind::Press {
-                                    _sender.send(Event::Key(key)).unwrap();
+                                    cloned_sender.send(Event::Key(key)).unwrap();
                                 }
                             },
                             CrosstermEvent::Resize(x, y) => {
-                                _sender.send(Event::Resize(x, y)).unwrap();
+                                cloned_sender.send(Event::Resize(x, y)).unwrap();
                             },
                             CrosstermEvent::Mouse(_) => { },
                             CrosstermEvent::FocusLost => { },
@@ -78,6 +80,13 @@ impl EventHandler {
             receiver,
             handler,
         }
+    }
+
+    pub async fn send(&mut self, event: Event) -> AppResult<()> {
+        self.sender.clone().send(event)
+            .or(Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::Other, "This is an IO error",
+            ))))
     }
 
     pub async fn next(&mut self) -> AppResult<Event> {
